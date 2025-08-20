1d86ae1bf45a73eb75e6654d40731a84f2df5ed7
Chris Mismash <your-email@example.com>
2025-08-20 00:02:49 -0400

    chore: consolidate site to docs/ only

--- apps/door-web/config.js
+++
@@ -1,5 +0,0 @@
-window.KNOCK_CONFIG = {
-  SUPABASE_URL: "https://eoqjdyyvcrflaczdostx.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvcWpkeXl2Y3JmbGFjemRvc3R4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1NzI3MzAsImV4cCI6MjA3MTE0ODczMH0.eCFoNfa1Fj6cd4avKd_-Q9WxcRc2ciWkYzsGx-Nu6A0"
-};
-
